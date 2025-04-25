b4793136d0e4ffda3f1704e14215ad2b9fc3b3f0
Doria Castañeda <doria.castaneda5060@alumnos.udg.mx>
2025-04-25 17:18:07 -0600

    Added Styles and visibility functionality

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const inputElement = document.getElementById('input');
 const outputElement = document.getElementById('output');
 
-// Le decimos al input que escuche cuando se presiona una tecla
+// Add an event listener to detect when a key is pressed inside the input field
 inputElement.addEventListener('keypress', function(e) {
 
-  // Verificamos si la tecla presionada fue la tecla "Enter"
+  // Check if the pressed key was the "Enter" key
   if (e.key === 'Enter') {
 
-    // Obtenemos el valor que el usuario escribió en el input
+    // Get the value entered by the user
     const movie = inputElement.value;
 
-    // Creamos la URL para hacer la solicitud a la API de OMDb
-    // Insertamos el nombre de la película en la URL con ${movie}
+    // Create the URL to request data from the OMDb API
+    // Insert the movie name into the URL using ${movie}
     const apiUrl = `https://www.omdbapi.com/?t=${movie}&apikey=c5483806`;
 
-    // Mostramos la URL en la consola para verificar que esté bien construida
+    // Print the URL in the console to check if it is correct
     console.log(apiUrl);
    
 
@@ -33,6 +33,12 @@ inputElement.addEventListener('keypress', function(e) {
       document.getElementById("title").innerText = data.Title;
       document.getElementById("plot").innerText = data.Plot;
       document.getElementById("actors").innerText = data.Actors;
+
+      //Visibility
+      document.getElementById("poster").style.visibility = "visible";
+      document.getElementById("title").style.visibility = "visible";
+      document.getElementById("plot").style.visibility = "visible";
+      document.getElementById("actors").style.visibility = "visible";
     })
     .catch(error => {
       console.error('Error:', error);
